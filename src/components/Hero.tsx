2588bf317eafe15c0bf8f15ec1edcde043172fc7
mutheeEverlyn <muthee.14952@students.kyu.ac.ke>
2025-04-01 18:34:42 +0300

    changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div>
       <div className="w-full max-w-full min-h-96 hero bg-cover bg-center bg-no-repeat" style={{backgroundImage:`url(${hero})`}}>
         <div className="hero-content text-center text-white">
-          <h1 className="text-4xl font-bold py-12 pb-1.5 animate-typing">Welcome to our website</h1>
+          <h1 className="text-4xl font-bold py-12 pb-1.5 animate-typing">Welcome to ALLNONEASSETS</h1>
           <p className="text-lg mt-0">We provide the best services for land,house and vehicle properties</p>
           <button className="bg-gray-400 hover:bg-amber-300 text-white font-bold py-2 px-4 rounded mt-4 *:transition duration-500"><Link to="/about-us">Learn More</Link></button>
           </div>
